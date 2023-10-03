00beac7deb7baaa13024440552888db279f95bf8
HANDZCZ <handz@email.cz>
2023-10-03 19:16:02 +0200

    Add vnc to docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-Xvfb $DISPLAY -screen $DISPLAY 1280x1024x16 & export DISPLAY=:99
-python ./src/main.py --nomenu --nowait --docker --profiles default
+# remove lock on virtual screen
+rm -f /tmp/.X1-lock
+# remove locks on chrome profiles (allows chrome to start without user input)
+rm -f /app/src/profiles/*/SingletonLock
+# remove google tmp files (prevents auto logout hopefully)
+rm -rf /tmp/.com.google.Chrome.*
+export DISPLAY=:1
+Xvfb $DISPLAY -screen 0 1280x1024x16 &
+x11vnc -display $DISPLAY -bg -forever -nopw -quiet -listen any -xkb -passwd $PASSWORD &
+python ./src/main.py --nomenu --nowait --docker
